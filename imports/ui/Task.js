767708202f73939210ea3a543bb37b7511bcfd5d
PedroPini <augustopini@gmail.com>
2023-09-13 13:04:42 +0800

    Initial commit

---
+++ imports/ui/Task.js
@@ -0,0 +1,18 @@
+import { Template } from 'meteor/templating';
+
+import { TasksCollection } from "../db/TasksCollection";
+
+import './Task.html';
+
+Template.task.events({
+  'click .toggle-checked'() {
+    // Set the checked property to the opposite of its current value
+    Meteor.call('tasks.setIsChecked', this._id, !this.isChecked);
+  },
+
+  'click .delete'(){
+    Meteor.call('tasks.remove', this._id);
+  },
+});
+
+
